fe5155b123b23f2873c8212fe3edb1c8ef9d5b2f
Agam Sharan Gupta <75520558+agamgupta2015@users.noreply.github.com>
2024-08-03 11:40:52 +0530

    Update flights.js

--- routes/flights.js
+++ routes/flights.js
@@ -6,8 +6,8 @@ const Notification = require('../models/notify')
 const twilio = require('twilio');
 
 // Wrong Credentials
-const accountSid = "AC054493b7213ac9355c19c5c3d2ac518";
-const authToken = "b6a1de89053aa23cebbe4012ea401e0";
+const accountSid = "AC054493b7213ac9355c19c5c3db2ac518";
+const authToken = "b6a1de89053aa23cebbe4012ea4b01e0";
 const twilioPhoneNumber = "+17626002931";
 
 const client = twilio(accountSid, authToken);
